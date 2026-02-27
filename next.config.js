4e844d821f7fadaffd4afc211e86aa2cf0fb21b6
claupeoliveira-sys <claupe.oliveira@gmail.com>
2026-02-27 17:26:15 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-module.exports = nextConfig
+const path = require('path');
+
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    return config;
+  },
+};
+
+module.exports = nextConfig;
