387a211701ae83d9e2558516f69f789580b4b300
claupeoliveira-sys <claupe.oliveira@gmail.com>
2026-02-27 17:20:20 -0300

    Create route.js

---
+++ app/api/features/[id]/route.js
@@ -0,0 +1,10 @@
+import connectDB from '@/lib/mongodb';
+import Feature from '@/lib/models/Feature';
+import { NextResponse } from 'next/server';
+
+export async function PUT(req, { params }) {
+  await connectDB();
+  const body = await req.json();
+  const feature = await Feature.findByIdAndUpdate(params.id, body, { new: true });
+  return NextResponse.json(feature);
+}
